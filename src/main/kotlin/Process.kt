781002b9bac03b8f78bd3f8a09c649f33a66bc6a
whitingpt <pete@whitings.org>
2018-12-18 21:18:12 -0700

    fixed the mess

--- src/main/kotlin/Process.kt
+++ src/main/kotlin/Process.kt
@@ -3,23 +3,47 @@ package org.whitings.bdayics
 import biweekly.ICalendar
 import biweekly.component.VEvent
 import biweekly.property.DateStart
+import biweekly.property.Description
+import biweekly.property.RecurrenceRule
 import biweekly.property.Summary
+import biweekly.util.Frequency
+import biweekly.util.Recurrence
 import java.io.FileReader
 import java.io.FileWriter
-import java.util.*
 import com.opencsv.bean.CsvToBeanBuilder
+import com.opencsv.bean.CsvBindByName
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class BdayList {
+    @CsvBindByName(column = "Preferred First Name")
+    val first: String? = null
+    @CsvBindByName(column = "Last Name")
+    val last: String? = null
+    @CsvBindByName(column = "Birthday")
+    val bday: String? = null
+    @CsvBindByName(column = "Work Email Address")
+    val email: String? = null
+    @CsvBindByName(column = "Business Title")
+    val title: String? = null
+    @CsvBindByName(column = "Direct Manager Name")
+    val manager: String? = null
+}
 
 fun main(args: Array<String>) {
     try {
-        if (args.size != 2) throw Exception("include csv with schedule as first argument and output file as second")
+        if (args.size != 2) throw Exception("include csv with birthday list as first argument and output file as second")
         val inputFilename = args[0]
         val outputFilename = args[1]
-        val chunks = CsvToBeanBuilder<ScheduleInfo>(FileReader(inputFilename)).withType(ScheduleInfo::class.java).build().parse()
+        val row = CsvToBeanBuilder<BdayList>(FileReader(inputFilename)).withType(BdayList::class.java).build().parse()
         val ical = ICalendar()
-        chunks.forEach { chunk ->
+        row.forEach { person ->
             ical.addEvent(VEvent().apply {
-                summary = Summary(chunk.scripture)
-                dateStart = DateStart(GregorianCalendar(2018, chunk.month - 1, chunk.day).time,false)
+                summary = Summary(person.first + " " + person.last?.take(1) + "'s birthday")
+                description = Description( "${person.email} ${person.title} manager: ${person.manager}")
+                var l = LocalDate.parse("${person.bday}/2018",DateTimeFormatter.ofPattern("MM/dd/yyyy"))
+                dateStart = DateStart(java.sql.Date.valueOf(l),false)
+                recurrenceRule = RecurrenceRule(Recurrence.Builder(Frequency.YEARLY).build())
             }
             )
         }
